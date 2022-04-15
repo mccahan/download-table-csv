c17ef9dd2b0fbf0fc107be25b4ecf78f22a90015
Ryan McCahan <ryan@mccahan.net>
2022-04-15 15:12:28 -0600

    Add option for a custom header row
    Because the BLS' header is garbled

--- README.md
+++ README.md
@@ -12,9 +12,10 @@ Downloads websites and outputs a table as a CSV.
 
 Options:
 - `-c` to require the rows to have the same number of columns as the first row. Helps strip out extra rows, e.g. footnotes at the end.
+- `-h` to replace the first row with custom column names
 
 ## Example
 
 Download the [Local Area Unemployment Statistics](https://www.bls.gov/web/metro/laummtrk.htm) figures by MSA from the U.S. Bureau of Labor Statistics
 
-`get-table-csv https://www.bls.gov/web/metro/laummtrk.htm '#laummtrk' -c`
+`get-table-csv https://www.bls.gov/web/metro/laummtrk.htm '#laummtrk' -c -h "MSA,UNEMPLOYMENT_RATE,UNEMPLOYMENT_RANK"`
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ program
   .argument('<url>', 'remote URL to download')
   .argument('<selector>', 'CSS selector for table')
   .option('-c, --require-head-columns', 'require rows to have the same number of columns as the header')
+  .option('-h, --header-row <header>', 'replace header row with custom text')
 
 program.parse()
 const options = program.opts()
@@ -35,7 +36,16 @@ axios.get(program.args[0])
     for (const row of table.rows) {
       let values = []
 
-      if (headerRowCount == 0) headerRowCount = row.cells.length
+      // If we're on the first row...
+      if (headerRowCount == 0) {
+        // Save the column count for the "-c" option
+        headerRowCount = row.cells.length
+
+        if (options.headerRow) {
+          process.stdout.write(options.headerRow + "\n")
+          continue
+        }
+      }
 
       for (const cell of row.cells) {
         const string = stripHtml(cell.innerHTML).result
